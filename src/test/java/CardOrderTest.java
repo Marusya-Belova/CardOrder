f1b2b8b990182a623d8262b60df28e5cd9a685f4
Marusya-Belova <m9175797050@gmail.com>
2022-02-27 21:03:50 +0300

    add CardOrderTest

---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+
+    @Test
+    void shouldReturnOK() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Белова Мария");
+        form.$("[data-test-id='phone'] input").setValue("+79175797050");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button").click();
+        $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена!"));
+    }
+}
